63a3541b26033ccee44bec2b95e2d9f9cbb9566d
Rix95 <ricalrax@gmail.com>
2023-11-13 21:52:09 -0600

    Added 80% parseLines functionality
    This adds upon further logic, still missing some corner cases.

--- UmlObject/UmlObject.java
+++ UmlObject/UmlObject.java
@@ -1,7 +1,10 @@
 package UmlObject;
+
+import java.util.ArrayList;
+
 public class UmlObject {
 
-    public UmlObject(String[] classStringArray){
+    public UmlObject(ArrayList<String> classStringArrayList, String type){
 
     }
 }
--- UmlParser.java
+++ UmlParser.java
@@ -4,13 +4,15 @@ import LanguageObjects.PythonObject;
 import UmlObject.UmlObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class UmlParser {
     String languageChoice;
     String [] lineArray;
-    UmlObject[] umlObjectArray ;
+    //ArrayList<UmlObject> umlObjectArray Currently not being used, To be deleted;
     LanguageObject[] languageObjectArray;
+    HashMap<String, UmlObject> umlObjectHashMap = new HashMap<>();
 
     public UmlParser(String inputUml, String languageChoice){
         this.languageChoice = languageChoice;
@@ -18,25 +20,63 @@ public class UmlParser {
         //TODO
     }
 
+    //TODO Eliminate in between blank lines
     /* Convert input to an array of line strings */
     public String[] parseInput(String inputUml) {
         return inputUml.split("\n");
     }
 
-    //TODO
-    public UmlObject[] parseLines(String [] lineArray){
+    //TODO Add constructor Logic as well.
+    public ArrayList<UmlObject> parseLines(String [] lineArray){
         //categorize each line, could be class, categorize behavior
-        return null;
+        ArrayList<String> localUmlElementArray = new ArrayList<>();
+        ArrayList<UmlObject> localUmlObjectArray = new ArrayList<>();
+        boolean isWithinClass = false;
+
+        for(String line: lineArray){
+            String[] words = line.split("\\s+");
+            //Found a class
+            if(words[0].equals("class")){
+                isWithinClass = true;
+                localUmlElementArray.add(words[1]);
+            }
+            //Found a comment
+            else if (words[0].equals("''")){
+                if(isWithinClass){
+                    localUmlElementArray.add(line);
+                }
+                else {
+                    localUmlObjectArray.add(new UmlObject(localUmlElementArray,"comment"));
+                }
+            }
+
+
+            else if (isWithinClass){
+                localUmlElementArray.add(line);
+            }
+            //End of class, delete localUmlElement, add to class as well
+            else if (words[0].equals("}")){
+                localUmlObjectArray.add(new UmlObject(localUmlElementArray, "class"));
+                localUmlElementArray.clear();
+            }
+            //If not categorzied, this will ignore intentionally done for
+        }
+
+
+
+
+        return localUmlObjectArray;
     }
 
     //TODO
-    public LanguageObject[] parseObject(UmlObject[] umlObjectArray, String languageChoice){
+    public LanguageObject[] parseUmlObject(ArrayList<UmlObject> umlObjectArray, String languageChoice){
+        //TODO Based on Language
+
         return null;
     }
-    //TODO Based on Language
 
     public Object[] decodeLanguageObject(UmlObject[] umlObjectArray, String languageChoice){
-        List<LanguageObject> languageObjectList = new ArrayList<>();
+        ArrayList<LanguageObject> languageObjectList = new ArrayList<>();
         for(UmlObject umlObject : umlObjectArray){
             if (languageChoice.toLowerCase().equals("java")){
                 languageObjectList.add(new JavaObject(umlObject));
