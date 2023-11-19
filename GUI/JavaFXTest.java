ef2b84a9ac4f49c64cf6c8c8229418a12ef4a0bc
Ben Underdahl <benunderdahl@gmail.com>
2023-11-19 16:29:52 -0600

    Added Buttons and file path selector that also displays file path

--- GUI/JavaFXTest.java
+++ GUI/JavaFXTest.java
@@ -1,33 +1,67 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.paint.Paint;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.scene.layout.*;
 
 public class JavaFXTest extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Create a button
 
-        Button btn = new Button("Click me");
+        primaryStage.setTitle("UML To Source Code");
 
-        // Set an action for the button
-        btn.setOnAction(e -> System.out.println("Button clicked!"));
+        // Create a FileChooser
+        FileChooser fileChooser = new FileChooser();
+        VBox left = new VBox(10);
 
-        // Create a layout and add the button to it
+        // Radio buttons for JAVA and PYTHON in hbox
+        HBox radio = new HBox();
+        RadioButton python = new RadioButton("Python");
+        RadioButton java = new RadioButton("Java");
+        radio.getChildren().addAll(python, java);
+        Label filePathLabel = new Label("Selected File: ");
+        filePathLabel.setPadding(new Insets(20, 20, 20, 20));
+        left.setPadding(new Insets(20));
+
+        // Buttons for Saving and Running Program
+        Button generate = new Button();
+        Button save = new Button();
+        generate.setText("Generate Class Diagram");
+        save.setText("Save Class Diagram");
+        generate.setTextFill(Paint.valueOf("White"));
+        generate.setBackground(Background.fill(Paint.valueOf("Blue")));
+        save.setTextFill(Paint.valueOf("White"));
+        save.setBackground(Background.fill((Paint.valueOf("Red"))));
+        HBox buttons = new HBox();
+        buttons.setAlignment(Pos.BOTTOM_LEFT);
+        buttons.getChildren().addAll(generate, save);
+
+        // Set extension filters
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Show the file dialog when a button is clicked
+        javafx.scene.control.Button openButton = new javafx.scene.control.Button("Open File Dialog");
+        openButton.setOnAction(e -> {
+            fileChooser.setTitle("Open File");
+            java.io.File selectedFile = fileChooser.showOpenDialog(primaryStage);
+            if (selectedFile != null) {
+                filePathLabel.setText(selectedFile.getAbsolutePath());
+            }
+        });
+        left.getChildren().addAll(radio,filePathLabel, openButton,buttons);
         javafx.scene.layout.StackPane root = new javafx.scene.layout.StackPane();
-        root.getChildren().add(btn);
-
-        // Create the scene
-        Scene scene = new Scene(root, 300, 250);
-
-        // Set the stage title and scene
-        primaryStage.setTitle("JavaFX Sample");
-        primaryStage.setScene(scene);
-
-        // Show the stage
+        root.getChildren().addAll(left);
+        primaryStage.setScene(new javafx.scene.Scene(root, 800, 500));
         primaryStage.show();
     }
 
