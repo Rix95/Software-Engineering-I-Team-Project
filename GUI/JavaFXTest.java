c4d3f35b8e1261332d8746df36241c97d6e48d8c
s550turbo <Benunderdahl@gmail.com>
2023-12-01 14:32:57 -0600

    added save function
    added 2 new functions for save button

--- GUI/JavaFXTest.java
+++ GUI/JavaFXTest.java
@@ -9,9 +9,11 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.layout.*;
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.List;
+import java.io.FileWriter;
 
 public class JavaFXTest extends Application {
 
@@ -19,6 +21,7 @@ public class JavaFXTest extends Application {
     private TextArea outputTextArea;
     private Label filePathLabel;
     private File selectedFile;
+    private File selectedSaveFile;
 
     @Override
     public void start(Stage primaryStage) {
@@ -31,7 +34,7 @@ public class JavaFXTest extends Application {
         filePathLabel.setPadding(new Insets(20));
 
         //TODO FOR TESTING PURPOSES
-         outputTextArea.setEditable(false);
+        outputTextArea.setEditable(false);
 //
         //TODO END
 
@@ -41,14 +44,14 @@ public class JavaFXTest extends Application {
         Label output = new Label();
         input.setText("Input");
         output.setText("Output");
-        grid.setMaxSize(800,800);
+        grid.setMaxSize(800, 800);
         grid.setVgap(20);
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(20);
         grid.add(input, 0, 0);
         grid.add(output, 1, 0);
-        grid.add(inputTextArea,0,1);
-        grid.add(outputTextArea,1,1);
+        grid.add(inputTextArea, 0, 1);
+        grid.add(outputTextArea, 1, 1);
         inputTextArea.setPrefSize(350, 400);
         outputTextArea.setPrefSize(350, 400);
 
@@ -76,7 +79,6 @@ public class JavaFXTest extends Application {
         // Buttons for Saving and Running Program
         Button load = new Button("Load");
         Button save = new Button("Save");
-        save.setDisable(true);
         Button generate = new Button("Generate");
         Button openButton = new Button("Select UML file (.txt)");
 
@@ -90,12 +92,11 @@ public class JavaFXTest extends Application {
         load.setTextFill(Paint.valueOf("White"));
         load.setBackground(Background.fill(Paint.valueOf("Blue")));
 
-        // Show the file dialog when Open button is clicked
+        // All the functions for the buttons are handled here
         openButton.setOnAction(e -> {
             openFile();
         });
 
-        // display text file in input when load button pressed
         load.setOnAction(e -> {
             loadFile();
         });
@@ -103,6 +104,11 @@ public class JavaFXTest extends Application {
         generate.setOnAction(e -> {
             generateCode();
         });
+
+        save.setOnAction(e -> {
+            saveFile();
+        });
+
 
         // Set extension filters
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt");
@@ -118,9 +124,10 @@ public class JavaFXTest extends Application {
         // adding components to the main node
         StackPane root = new StackPane();
         root.getChildren().add(grid);
-        primaryStage.setScene(new javafx.scene.Scene(root,800,800));
+        primaryStage.setScene(new javafx.scene.Scene(root, 800, 800));
         primaryStage.show();
-}
+    }
+
     // When the load button is pressed this method will be
     // called displaying the text file Into the text area object
     private void loadFile() {
@@ -140,13 +147,11 @@ public class JavaFXTest extends Application {
             }
         }
     }
+
     private void generateCode() {
         UmlParser umlParser = new UmlParser(inputTextArea.getText(), "java");
         outputTextArea.setText(umlParser.parseCodeIntoString());
-
-
     }
-
 
     // made this method to display the filepath when the select a file button is pressed
     private void openFile() {
@@ -156,8 +161,27 @@ public class JavaFXTest extends Application {
         if (selectedFile != null) {
             filePathLabel.setText("Selected File: " + selectedFile.getAbsolutePath());
         }
-
     }
+
+    private void saveFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save File");
+        selectedSaveFile = fileChooser.showSaveDialog(null);
+
+        if (selectedSaveFile != null) {
+            saveToFile(selectedSaveFile, outputTextArea.getText());
+            filePathLabel.setText("Selected File: " + selectedSaveFile.getAbsolutePath());
+        }
+    }
+
+    private void saveToFile(File file, String content) {
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
 
